4014229f87fedda1972123f71350e3eb7dca9244
X_Shadow_ <33694505+shadowdevfr@users.noreply.github.com>
2022-08-21 17:29:23 +0200

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -51,7 +51,7 @@ export const tools = [
 export const projects = [
     {
       title: "Wolfer",
-      subtitle: "React Native (expo) game. - React frontend, Node.js API & websocket backend.",
+      subtitle: "React Native (expo) game. - React frontend, Node.js API & websocket backend. - FullStack",
       description:
         "Wolfer is an adapation of the famous card game, ''The werewolves of Millers Hollow''. It is a multiplayer game where players try to eliminate their enemy, according to their cards. The game is played on a mobile device, and will be available on both iOS and Android. The game is developed using React Native, Expo, Node.js, Express, Websockets, Redis, MySQL & PHP.",
       image: "https://i.imgur.com/ZBZyKYQ.png",
@@ -59,7 +59,7 @@ export const projects = [
     },
     {
       title: "OptikServers.com",
-      subtitle: "Founder, assiting in the development of the dashboard (PHP), made the Discord bot (Discord.js), made the Android App.",
+      subtitle: "Discord bot, Android app, and client panel (PHP)",
       description:
         "I'm one of the founders of OptikServers.com, a service that provides free 24/7 hosting services for Minecraft servers, Discord bots and more. I was in charge of the development of the Discord bot, and participated in the developement of the client area.",
       image: "https://i.imgur.com/aDDNaPN.png",
@@ -67,7 +67,7 @@ export const projects = [
     },
     {
       title: "QuackHost.uk",
-      subtitle: "PHP client panel for free hosting services.",
+      subtitle: "PHP",
       description:
         "A complete home-made client panel using PHP and MySQL. Using a dashboard template from Creative Tim, the ui & ux is easily understandable and easy to use. Coin AFK earning, server management & creation, economy system with a shop, and a lot more.",
       image: "https://i.imgur.com/U1X5mYQ.png",
@@ -75,10 +75,18 @@ export const projects = [
     },
     {
         title: "Shadow's Dash",
-        subtitle: "A PHP client panel for hosting services.",
+        subtitle: "PHP",
         description:
             "A fully customizable shared-resources based dashboard for pterodactyl hosting services. It is easily customizable, and is meant to be used with Pterodactyl. It allows your users to manage, create and delete their servers. Using a queue system, you can create a queue for servers created by your users, and manage the slots assigned to each node. It comes with a full economy system, with a shop for users to buy resources.",
         image: "https://i.imgur.com/VezCVyn.png",
         link: "https://github.com/ShadowsDash/ShadowsDash",
+    },
+    {
+        title: "PepsiTube",
+        subtitle: "FullStack",
+        description:
+            "An easy and fast YouTube video downloader powered by a yt-dlp API backend. No ads, no pop-ups.",
+        image: "https://i.imgur.com/i2pWoLq.png",
+        link: "https://tube.pepsi.host",
     },
   ];
