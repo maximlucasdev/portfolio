5a1680506a9cb8e00f15b18cf2b8a70530250e3e
X_Shadow_ <33694505+shadowdevfr@users.noreply.github.com>
2022-11-02 19:12:15 +0100

    Added status page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,6 +14,7 @@ export default function Navbar() {
                 <a href="#skills" className="mr-5 hover:text-white">Skills</a>
                 <a href="#projects" className="mr-5 hover:text-white">Projects</a>
                 <a href="#contact" className="mr-5 hover:text-white">Contact</a>
+                <a href="//status.xshadow.xyz" className="mr-5 hover:text-white">Services status</a>
             </nav>
         
         </div>
