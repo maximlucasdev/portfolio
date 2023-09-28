d9090cf11cbb216efc0679f0f0331e1914aa7d9c
shadowdevfr <contact@xshadow.xyz>
2023-09-28 18:43:02 +0200

    Fix: language issue & icons align

--- src/Desktop.tsx
+++ src/Desktop.tsx
@@ -44,7 +44,7 @@ export default function Desktop() {
           setShowContextMenu(false);
         }}>
             <Suspense fallback={<></>}><AppsWindowsManager/></Suspense>
-            <div class='flex flex-col h-full flex-wrap gap-2 p-2 md:p-5 w-screen'>
+            <div class='flex flex-col h-full flex-wrap gap-2 p-2 md:p-5 w-screen content-start'>
                 {Apps.map((app) => {
                   if (app.name === "Pepsi" && !pepsimode.value) return null;
                   if (app.hide) return null;
