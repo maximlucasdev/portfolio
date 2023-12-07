38c9a9c1eded9ef7be6b3bc43f2cfffc4c2e1062
shadowdevfr <contact@xshadow.xyz>
2023-12-07 22:09:06 +0100

    fix

--- src/lib/DesktopAppIcon.tsx
+++ src/lib/DesktopAppIcon.tsx
@@ -3,7 +3,7 @@ import { openApp } from "./AppsWindowsManager";
 export default function DesktopAppIcon(props:{app:App}) {
   return (
     <>
-      <button draggable={false} class='rounded-md w-24 min-h-max flex flex-col items-center justify-center gap-1 md:p-2 hover:shadow-md hover:bg-blue-500/20 transition border-[1px] border-transparent hover:border-blue-500/20' onClick={() => {
+      <button draggable={false} class='rounded-md z-10 w-24 min-h-max flex flex-col items-center justify-center gap-1 md:p-2 hover:shadow-md hover:bg-blue-500/20 transition border-[1px] border-transparent hover:border-blue-500/20' onClick={() => {
         openApp(props.app)
       }}
       onDrag={(e) => {
