018f29dae13883cdd41dc5ae90ff63f16e10e373
shadowdevfr <contact@xshadow.xyz>
2023-12-07 22:25:19 +0100

    remove test value

--- src/Desktop.tsx
+++ src/Desktop.tsx
@@ -75,7 +75,7 @@ export default function Desktop() {
             {showWelcome ? (<div ref={refs.setFloating} style={floatingStyles}><WelcomePopup hide={() => {setShowWelcome(false)}}/></div>) : null}
             {/* @ts-ignore */}
             <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDuration={5000} isVisible={!isAppFullscreen.value}>
-              <p class='absolute bottom-12 md:bottom-24 right-0 text-xs pr-2 pb-2 md:pb-0 md:pr-10 md:text-sm text-end text-white opacity-20'>{activeWindow.value} © {new Date().getFullYear()} <a href='https://github.com/shadowdevfr' class='hover:opacity-50 transition' target='_blank'> Maxim Lucas</a> {pepsimode.value ? <span class='text-xl'><br/>🐈 Pepsi Mode</span> : ''}</p>
+              <p class='absolute bottom-12 md:bottom-24 right-0 text-xs pr-2 pb-2 md:pb-0 md:pr-10 md:text-sm text-end text-white opacity-20'>© {new Date().getFullYear()} <a href='https://github.com/shadowdevfr' class='hover:opacity-50 transition' target='_blank'> Maxim Lucas</a> {pepsimode.value ? <span class='text-xl'><br/>🐈 Pepsi Mode</span> : ''}</p>
             </Animated>
             
             <div ref={refs.setReference} class='fixed bottom-0 w-screen z-50'>
