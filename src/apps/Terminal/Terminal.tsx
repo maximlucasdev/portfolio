a201c425c6a62780e9a4b6e919b3c4171d45b92f
shadowdevfr <contact@xshadow.xyz>
2024-01-06 13:02:43 +0100

    terminal fixes

--- src/apps/Terminal/Terminal.tsx
+++ src/apps/Terminal/Terminal.tsx
@@ -21,7 +21,7 @@ export default function Terminal() {
           <div className='flex flex-col justify-end'>
             <div className='font-mono w-full h-full md:h-96 p-3 text-white overflow-y-scroll noscrollbar' style={{height: window.innerWidth < 768 ? window.innerHeight-100 : 385}} id={termId}>
               <p className='text-gray-400 mb-5'><span class='gradient-text'>Welcome to PortfoliOS 1.0.0 (x64)</span><br/><br/>
-                * Website:  https://xshadow.xyz<br/>
+                * Website:  https://xshadow.xyz | https://maximlucas.dev<br/>
                 * Support:  hello@xshadow.xyz
                 </p>
               <p className='text-gray-500 text-xs'>(i) Type "help" to start! To type commands, click on "help" below. It's the input box!</p>
@@ -34,13 +34,13 @@ export default function Terminal() {
                 }}/>
               })}
               <p>
-                visitor@xshadow:~$ 
-                <input id={termInputId} className='ml-2 bg-zinc-800 focus:outline-none' style={{width: `${inputData.length === 0 ? 4 : inputData.length}ch`}} placeholder='help' value={inputData} onChange={(value) => {inputHandler(value.target.value)}} onKeyDown={(key) => {
+                visitor@maxluc:~$ 
+                <input id={termInputId} className='ml-2 bg-zinc-800 focus:outline-none' style={{width: inputData.length === 0 ? 1 : `${inputData.length}ch`}} placeholder='' value={inputData} onChange={(value) => {inputHandler(value.target.value)}} onKeyDown={(key) => {
                     if (key.keyCode === 13) { // had to use deprecated keyCode because it doesn't work on some mobile devices
                       setInputData(inputData.toLowerCase().trimEnd());
                       if (inputData.length === 0) return;
                       const command = commands.filter((value) => value.name === inputData)[0];
-                      const comp = () => {return <p>visitor@xshadow:~$ {inputData}</p>}
+                      const comp = () => {return <p>visitor@maxluc:~$ {inputData}</p>}
                       if (!command) {
                         setHistory([...history, comp, () => {return <p className='text-red-500'>{inputData}: command not found</p>}]);
                         setInputData('');
