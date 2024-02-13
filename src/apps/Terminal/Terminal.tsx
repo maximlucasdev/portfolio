e66cb1046f5c2698849007359b4b95988534e2d7
shadowdevfr <contact@xshadow.xyz>
2024-02-13 19:09:50 +0100

    Added terminal command history

--- src/apps/Terminal/Terminal.tsx
+++ src/apps/Terminal/Terminal.tsx
@@ -9,6 +9,8 @@ export default function Terminal() {
     const [inputData, setInputData] = useState('');
     const [completion, setCompletion] = useState('');
     const [history, setHistory] = useState([]);
+    const [inputHistory, setInputHistory] = useState([]);
+    const [historyCursor, setHistoryCursor] = useState(0);
     const inputHandler = (value:string) => {
         setInputData(value);
         setCompletion(autocomplete(value));
@@ -36,19 +38,37 @@ export default function Terminal() {
               <p>
                 visitor@maxluc:~$ 
                 <input id={termInputId} className='ml-2 bg-zinc-800 focus:outline-none' style={{width: inputData.length === 0 ? 1 : `${inputData.length}ch`}} placeholder='' value={inputData} onChange={(value) => {inputHandler(value.target.value)}} onKeyDown={(key) => {
-                    if (key.keyCode === 13) { // had to use deprecated keyCode because it doesn't work on some mobile devices
-                      setInputData(inputData.toLowerCase().trimEnd());
-                      if (inputData.length === 0) return;
-                      const command = commands.filter((value) => value.name === inputData)[0];
-                      const comp = () => {return <p>visitor@maxluc:~$ {inputData}</p>}
-                      if (!command) {
-                        setHistory([...history, comp, () => {return <p className='text-red-500'>{inputData}: command not found</p>}]);
-                        setInputData('');
-                      } else {
-                        setHistory([...history, comp, command.Response]);
-                        setInputData('');
-                      }
-                      setTimeout(() => document.getElementById(termId).scrollTop = document.getElementById(termId).scrollHeight, 100);
+                    switch (key.keyCode) { // Had to use deprecated keycode to prevent compatibility issues
+                      case 13:
+                        setInputData(inputData.toLowerCase().trimEnd());
+                        const newInputHistory = [...inputHistory, inputData]; // Had to use this to avoid delays
+                        setInputHistory(newInputHistory);
+                        setHistoryCursor(newInputHistory.length);
+                        if (inputData.length === 0) return;
+                        const command = commands.filter((value) => value.name === inputData)[0];
+                        const comp = () => {return <p>visitor@maxluc:~$ {inputData}</p>}
+                        if (!command) {
+                          setHistory([...history, comp, () => {return <p className='text-red-500'>{inputData}: command not found</p>}]);
+                          setInputData('');
+                        } else {
+                          setHistory([...history, comp, command.Response]);
+                          setInputData('');
+                        }
+                        setTimeout(() => document.getElementById(termId).scrollTop = document.getElementById(termId).scrollHeight, 100);
+                        break;
+                      case 38:
+                        if (!inputHistory[historyCursor-1]) break;
+                        setInputData(inputHistory[historyCursor-1]);
+                        setHistoryCursor(historyCursor-1);
+                        break;
+                      case 40:
+                        if (!inputHistory[historyCursor+1]) break;
+                        setInputData(inputHistory[historyCursor+1]);
+                        setHistoryCursor(historyCursor+1);
+                        break;
+                      default:
+                        setHistoryCursor(inputHistory.length);
+                        break;
                     }
                 }}></input>
                 <span className='text-gray-400 mr-1'>{completion}</span>
